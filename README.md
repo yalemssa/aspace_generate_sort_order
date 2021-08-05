ae19db055b907c7b40047ccbbcf739abfa299400
ucancallmealicia <alicia.detelich@yale.edu>
2021-08-05 11:18:52 -0400

    add files

---
+++ README.md
@@ -0,0 +1,77 @@
+## aspace_generate_sort_order
+
+A lightweight Python application to generate a custom sort order from ArchivesSpace. 
+
+### What does this application do?
+
+The application performs the following tasks on each row of a CSV report containing information about all archival objects within a given collection:
+
+1. Retrieves the JSON record for the object
+2. Extracts the `ancestor` array from the JSON and reverses it so that the collection-level record is first, etc.
+3. Retrieves JSON for each ancestor, excluding the collection-level record, as resource records do not have position values
+4. Extracts the `position` value from each ancestor and, if the position value is less than 5 digits in length, add leading zeros. The largest collection in ArchivesSpace has ~88000 records linked to it, meaning that even if the collection were totally flat there would not be a position value greater than 5 digits in length
+5. Concatenate each position value to a string variable, with a dot after each position. The sort order will end up looking something like this: 00001.00027.00005.00435
+6. Adds this position value in a new column in the input CSV file
+
+### Requirements
+
+- Running the executable file requires a Mac. You do not need to install Python or any other dependencies. You can just double-click on the file to run the script.
+- Running the Python script requires Python 3.8+ and the `requests` and `rich` third-party libraries
+
+### Tutorial
+
+#### Generating the input file
+
+This application takes the custom `All Archival Objects` report (CSV format) as input. This report is generated within the ArchivesSpace staff interface. To run this report: 
+
+1. Click on the gear icon next to the repository name in the staff interface
+2. Select `Reports` from the drop down
+3. Click on the `All Archival Objects` report
+4. Enter the call number of the collection in the `Call Number` box
+5. Select `CSV` from the `Format` dropdown menu
+6. Click `Start Job` to start the report job
+7. When the report finishes, click the `Refresh Page` button
+8. Click the `Download Report` link to download the report
+
+#### Configuration settings
+
+The application comes with a `config.json` file, which allows the user to specify ArchivesSpace login information and the path to the input CSV file.
+
+Sample `config.json` formatting:
+
+```
+{
+	"input_csv": "full/path/to/input_csv.csv",
+	"aspace_api_url": "https://archivesspace.library.yale.edu/api",
+	"aspace_username": "yourusername",
+	"aspace_password": "yourpassword"
+}
+```
+
+If the configuration file is not completed, the application will prompt the user for each of these inputs.
+
+#### Running the application
+
+Double click on the executable file to run the script. If the configuration file is complete, the script will begin immediately. If not the user will be prompted to enter the input file path and ArchivesSpace login data.
+
+Depending on the number of archival objects associated with the collection, the script could take a while to run (test runs completed approximately 675 records per minute). A progress bar will appear which includes the number of records processed and the estimated time remaining.
+
+#### Output
+
+The application outputs a CSV file, and stores it in the same directory as the input file. The filename will be the same as the input file, with the addition of `_output` at the end of the filename - i.e. `full/path/to/input_csv_output.csv`
+
+The output CSV file includes a new column, `sort_order`, which stores the sort order that is generated during the process. The values in this column can be sorted within spreadsheet software, script, or other application as needed.
+
+__NOTE:__ If sorting in spreadsheet software, it is important to specifically open the file as plain text. Many spreadsheet software applications will default to `General` format, which can cause leading zeros to be dropped from the sort order values. Obviously if this happens the sort order will not work properly.
+
+If using Excel, follow these steps to ensure that the output file is opened in plain text:
+
+1. Open a blank workbook in Excel
+2. Select `Data > Get External Data > Import Text File...`
+3. In Step 1 of the Text Wizard, select the `Delimited` radio button and press `Next >`
+4. in Step 2, check the `Comma` box in the `Delimiters` menu
+5. In Step 3, scroll to the right on the preview until you can see the last column (sort order)
+6. Click on the last column to highlight it
+7. Select the `Text` radio button in the Column data format menu
+8. Click `Finish`
+9. Select the `Existing sheet` radio button from the next menu and click `OK`
---
+++ aspace_sort_order.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import csv
+import json
+import os
+import sys
+import traceback
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    TextColumn,
+    TransferSpeedColumn,
+    TimeRemainingColumn,
+    Progress,
+    TaskID,
+)
+
+#from tqdm import tqdm
+
+'''
+Gets a sort order/hierarchical position for each ArchivesSpace record in a spreadsheet.
+'''
+
+
+def login(url=None, username=None, password=None):
+    """Logs into the ArchivesSpace API"""
+    try:
+        if (url in (None, '')) or (username in (None, '')) or (password in (None, '')):
+            url = input('Please enter the ArchivesSpace API URL: ')
+            username = input('Please enter your username: ')
+            password = input('Please enter your password: ')
+        auth = requests.post(url+'/users/'+username+'/login?password='+password).json()
+        #if session object is returned then login was successful; if not it failed.
+        if 'session' in auth:
+            session = auth["session"]
+            h = {'X-ArchivesSpace-Session':session, 'Content_Type': 'application/json'}
+            print('Login successful!')
+            return (url, h)
+        else:
+            print('Login failed! Check credentials and try again.')
+            print(auth.get('error'))
+            #try again
+            u, heads = login()
+            return u, heads
+    except:
+        print('Login failed! Check credentials and try again!')
+        u, heads = login()
+        return u, heads
+
+def get_sort_order(ancestors, position, api_url, headers, sesh, uri):
+    sort_order = ''
+    for ancestor in ancestors:
+        # don't want to include the collection, since resource records don't have a position
+        if ancestor.get('level') != 'collection':
+            record_json = sesh.get(f"{api_url}{ancestor.get('ref')}", headers=headers).json()
+            record_position = str(record_json.get('position')).zfill(5)
+            sort_order = f"{sort_order}.{record_position}"
+    sort_order = f"{sort_order}.{str(position).zfill(5)}"
+    return sort_order.lstrip('.')
+
+def process_archival_objects(api_url, headers, sesh, row, progbar, master_task):
+    try:
+        progbar.advance(master_task, 1)
+        # URI is in the fourth row in the report - could change this?
+        uri = row[3]
+        record_json = sesh.get(f"{api_url}{uri}", headers=headers).json()
+        # gets the position of the base ao
+        position = record_json.get('position')
+        # reverses ancestor array so it's ordered from collection on down to the lowest level ao
+        ancestors = record_json.get('ancestors')[::-1]
+        sort_order = get_sort_order(ancestors, position, api_url, headers, sesh, uri)
+        # print(sort_order)
+        row.append(sort_order)
+        return row
+    except Exception:
+        print(traceback.format_exc())
+
+def write_results(futures, csvoutfile):
+    for future in futures:
+        result = future.result()
+        csvoutfile.writerow(result)
+
+def skip_report_rows(csvfile):
+    first_row = next(csvfile)
+    if first_row[0] == 'total_count':
+        for i in range(2):
+            next(csvfile)
+        first_row = next(csvfile)
+    return first_row
+
+def get_rowcount(fp):
+    with open(fp) as csvin:
+        return len(list(csv.reader(csvin))) - 4
+
+
+def rich_setup():
+    console = Console(record=True)
+    progress = Progress(
+        BarColumn(bar_width=None),
+        "[progress.percentage]{task.percentage:>3.1f}%",
+        "•",
+        "[progress.completed]{task.completed}/{task.total}",
+        "•",    
+        TimeRemainingColumn(),
+        console=console
+    )
+    return progress
+
+def main():
+    EXE_PATH = os.getcwd()
+    #EXE_PATH = sys.executable.replace('/aspace_sort_order', '')
+    progbar = rich_setup()
+    cfg = json.load(open(f"{EXE_PATH}/config.json"))
+    api_url, headers = login(url=cfg.get('aspace_api_url'), username=cfg.get('aspace_username'), password=cfg.get('aspace_password'))
+    if cfg.get('input_csv') in (None, ''):
+        input_fp = input('Enter path to input CSV: ')
+    else:
+        input_fp = cfg.get('input_csv')
+    output_fp = f"{input_fp.replace('.csv', '')}_output.csv"
+    rowcount = get_rowcount(input_fp)
+    master_task = progbar.add_task("overall", total=rowcount, filename="Overall Progress")
+    try:
+        with progbar, requests.Session() as sesh, ThreadPoolExecutor(max_workers=4) as pool, open(input_fp, 'r', encoding='utf8') as inputfile, open(output_fp, 'a', encoding='utf8') as outputfile:
+            futures = []
+            csvfile = csv.reader(inputfile)
+            header_row = skip_report_rows(csvfile)
+            csvoutfile =  csv.writer(outputfile)
+            csvoutfile.writerow(header_row + ['sort_order'])
+            for row in csvfile:
+                try:
+                    future = pool.submit(process_archival_objects, api_url, headers, sesh, row, progbar, master_task)
+                    futures.append(future)
+                except Exception:
+                    print(row)
+                    print(traceback.format_exc())
+            if futures:
+                write_results(futures, csvoutfile)
+    except Exception:
+        print(traceback.format_exc())
+
+
+if __name__ == "__main__":
+    main()
